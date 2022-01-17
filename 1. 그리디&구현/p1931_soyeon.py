7beb6aae44ca754e5a36ea30bc7159fa9ea283d2
Soyeon Min <skql1502@gmail.com>
2022-01-17 17:42:53 +0900

    Update p1931_soyeon.py

--- 1. 그리디&구현/p1931_soyeon.py
+++ 1. 그리디&구현/p1931_soyeon.py
@@ -4,7 +4,7 @@ for _ in range(n):
   meeting.append(list(map(int, input().split())))
 sorted_meetings = sorted(meeting, key=lambda x: (x[1], x[0]))
 avail = []
-avail.append(sorted_meetings[0])
+avail.append(sorted_meetings.pop(0))
 for sorted_m in sorted_meetings:
   size = len(avail)
   if(sorted_m[0]>=avail[size-1][1]):
